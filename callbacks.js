b2bbf5a683f12722e918ca351cced812592d8407
Stephane <son.trinh.dev@gmail.com>
2017-12-30 14:42:33 +0000

    automatically backed up by learn

--- callbacks.js
+++ callbacks.js
@@ -8,3 +8,30 @@ var nails = [
   'bent nail',
   'clean nail'
 ]
+for (let i in nails) {
+  clean(nails[i])
+}
+
+var planks = [
+  'splintered plank',
+  'straight plank',
+  'bent plank'
+]
+
+for (let k in planks) {
+  clean(planks[k])
+}
+
+console.log('done!')
+
+function groupAndClean(items, cleaningMethod, done) {
+  for (var i = 0; i < items.length; i++) {
+    cleaningMethod(items[i])
+  }
+  done()
+}
+
+groupAndClean(nails.concat(planks), clean, function()
+{
+  console.log('Whew, that was a lot of work')
+})
